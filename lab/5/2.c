a9e6264e20d3f7db50ceaa23af24f924040828ab
Milesq <wmilosz88@gmail.com>
2022-01-12 23:25:59 +0100

    lab 5 2

---
+++ lab/5/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef long long int lli;
+
+lli factorial_recursive(int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    return n * factorial_recursive(n - 1);
+}
+
+lli factorial_loop(int n) {
+    lli result = 1;
+
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
+void main() {
+    for (size_t i = 1; i <= 7; i++) {
+        printf("%d! = %d\n", i, factorial_recursive(i));
+        printf("%d! = %d\n\n", i, factorial_loop(i));
+    }
+}
