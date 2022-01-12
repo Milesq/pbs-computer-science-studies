ed8176ad5ac8e9d5c3b7f8ce7707d2ed2c724dab
Milesq <wmilosz88@gmail.com>
2022-01-12 23:25:55 +0100

    lab 5 1

---
+++ lab/5/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+
+int min(int x, int y) {
+    return x < y ? x : y;
+}
+
+void test_min() {
+    assert(min(0, 1) == 0);
+    assert(min(1, 0) == 0);
+    assert(min(0, 0) == 0);
+    assert(min(1, -1) == -1);
+    assert(min(0, -1) == -1);
+    assert(min(-1, 1) == -1);
+    assert(min(-1, 0) == -1);
+    assert(min(-1321, -213) == -1321);
+    assert(min(1321, 213) == 213);
+}
+
+void main() {
+    test_min();
+}
