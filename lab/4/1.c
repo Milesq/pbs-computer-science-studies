27cbc852c3a85417fc95be054d5c39b70e032c79
Milesq <wmilosz88@gmail.com>
2022-01-12 18:41:18 +0100

    add 4 1

---
+++ lab/4/1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+size_t count_words(const char* str) {
+    size_t count = 0;
+    bool in_word = false;
+
+    while (*str != '\0') {
+        if (isspace(*str)) {
+            in_word = false;
+        } else if (!in_word) {
+            in_word = true;
+            count++;
+        }
+
+        str++;
+    }
+
+    return count;
+}
+
+void main() {
+    char str[128];
+    fgets(str, sizeof(str), stdin);
+    printf("%d", count_words(str));
+}
